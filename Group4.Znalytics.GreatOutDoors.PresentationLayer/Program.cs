ac390a42feacc4f5f0ce75036940279b547b356b
Kalvaprasanna <prasannakalva8@gmail.com>
2020-08-14 23:05:55 +0530

    Small Program using If-else condition

--- Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
+++ Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
@@ -2,12 +2,21 @@
 {
     static void Main()
     {
-        
+        int a, b=20;
         
         System.Console.WriteLine("Hello World");
+        //System.Console.ReadKey();
+        System.Console.WriteLine("Enter a value");
+        a = int.Parse(System.Console.ReadLine());
+        if(a>b)
+        {
+            System.Console.WriteLine("a is bigger");
+        }
+        else
+        {
+            System.Console.WriteLine("b  is bigger");
+        }
         System.Console.ReadKey();
-
-
         
     }
 }
