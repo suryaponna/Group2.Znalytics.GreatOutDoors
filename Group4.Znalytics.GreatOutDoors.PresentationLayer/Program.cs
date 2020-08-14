53f4dbfe8ccd1126e7994e73489aba02d9cbff34
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-14 18:05:16 +0530

    name

--- Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
+++ Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Group4.Znalytics.GreatOutDoors.PresentationLayer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            System.Console.WriteLine("archana"); 
         }
     }
 }
