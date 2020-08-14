d82981085a661b2e6bb55dc8092803b97d65e317
meghana533 <meghananimishakavi199@gmail.com>
2020-08-14 14:23:39 +0530

    code

--- Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
+++ Group4.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Group4.Znalytics.GreatOutDoors.PresentationLayer
+﻿class program
 {
-    class Program
+    public void Main()
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
