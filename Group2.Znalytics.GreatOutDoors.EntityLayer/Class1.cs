05e3a6168d9342cda9322e5d9c484b4f9f06fab4
SuryaPonna <suryaponna0@gmail.com>
2020-08-15 10:34:04 +0530

    15-08-2020,10.33AM

--- Group2.Znalytics.GreatOutDoors.EntityLayer/Class1.cs
+++ Group2.Znalytics.GreatOutDoors.EntityLayer/Class1.cs
@@ -1,7 +1,7 @@
 ﻿/// <summary>
 /// Shippinng Address Details
 /// </summary>
-public class AddressDetail
+public class AddressDetails
 {
     private string _customerCountry;
     private string _customerFullName;
@@ -13,118 +13,21 @@ public class AddressDetail
     private string _townCity;
     private string _selState;
     private string _addressType;
-    //private bool _changeAddress = false;
+    private bool _changeAddress = false;
 
-    public string CustomerCountry
+namespace Group2.Znalytics.GreatOutDoors.EntityLayer
+{
+    public class Class1
     {
-        set
-        {
-            _customerCountry = value;
-        }
-        get
-        {
-            return _customerCountry;
-        }
     }
-    public string CustomerName
-    {
-        set
-        {
-            value = _customerFullName;
-        }
-        get
-        {
-            return this._customerFullName;
-        }
-    }
-    public long MobileNumber
-    {
-        set
-        {
-            this._mobileNumber = value;
-        }
-        get
-        {
-            return this._mobileNumber;
-        }
-    }
-    public long PinCode
-    {
-        set
-        {
-            this._pinCode = value;
-        }
-        get
-        {
-            return this._pinCode;
-        }
-    }
-    public string FlatNo
-    {
-        set
-        {
-            this._flatDno = value;
-        }
-        get
-        {
-            return this._flatDno;
-        }
-    }
-    public string AreaColony
-    {
-        set
-        {
-            this._areaColony = value;
-        }
-        get
-        {
-            return this._areaColony;
-        }
-    }
-    public string LandMark
-    {
-        set
-        {
-            this._landMark = value;
-        }
-        get
-        {
-            return this._landMark;
-        }
-    }
+    /// <summary>
+    /// Product Quantity
+    /// </summary>
 
-    public string Town
-    {
-        set
-        {
-            this._townCity = value;
-        }
-        get
-        {
-            return this._townCity;
-        }
-    }
-    public string State
-    {
-        set
-        {
-            this._selState = value;
-        }
-        get
-        {
-            return this._selState;
-        }
-    }
-    public string AddressType
-    {
-        set
-        {
-            this._addressType = value;
-        }
-        get
-        {
-            return this._addressType;
-        }
-    }
+    public int ProductQuantity { set; get; }
+    /// <summary>
+    /// Product Type
+    /// </summary>
+    public string Producttype { set; get; }
 
 }
