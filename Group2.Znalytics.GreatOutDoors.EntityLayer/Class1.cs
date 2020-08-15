c675164cfcb63cce76ac60948ec9d06ba06387c7
Kalvaprasanna <prasannakalva8@gmail.com>
2020-08-15 13:23:16 +0530

    modifications on returnsmodule

--- Group2.Znalytics.GreatOutDoors.EntityLayer/Class1.cs
+++ Group2.Znalytics.GreatOutDoors.EntityLayer/Class1.cs
@@ -1,22 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Group2.Znalytics.GreatOutDoors.EntityLayer
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Markup;
+/// <summary>
+/// Shippinng Address Details
+/// </summary>
+public class AddressDetails
 {
-    public class Class1
-    {
-    }
-    /// <summary>
-    /// Product Quantity
-    /// </summary>
+    private string _customerCountry;
+    private string _customerFullName;
+    private long _mobileNumber;
+    private long _pinCode;
+    private string _flatDno;
+    private string _areaColony;
+    private string _landMark;
+    private string _townCity;
+    private string _selState;
+    private string _addressType;
+    private bool _changeAddress = false;
 
-    public int ProductQuantity { set; get; }
-    /// <summary>
-    /// Product Type
-    /// </summary>
-    public string Producttype { set; get; }
+    public string CustomerCountry
+    {
+        set
+        {
+            _customerCountry = value;
+        }
+        get
+        {
+            return _customerCountry;
+        }
+    }
+    public string CustomerName
+    {
+        set
+        {
+            value = _customerFullName;
+        }
+        get
+        {
+            return this._customerFullName;
+        }
+    }
+    public long MobileNumber
+    {
+        set
+        {
+            this._mobileNumber = value;
+        }
+        get
+        {
+            return this._mobileNumber;
+        }
+    }
+    public long PinCode
+    {
+        set
+        {
+            this._pinCode = value;
+        }
+        get
+        {
+            return this._pinCode;
+        }
+    }
+    public string FlatNo
+    {
+        set
+        {
+            this._flatDno = value;
+        }
+        get
+        {
+            return this._flatDno;
+        }
+    }
+    public string AreaColony
+    {
+        set
+        {
+            this._areaColony = value;
+        }
+        get
+        {
+            return this._areaColony;
+        }
+    }
+    public string LandMark
+    {
+        set
+        {
+            this._landMark = value;
+        }
+        get
+        {
+            return this._landMark;
+        }
+    }
+
+    public string Town
+    {
+        set
+        {
+            this._townCity = value;
+        }
+        get
+        {
+            return this._townCity;
+        }
+    }
+    public string State
+    {
+        set
+        {
+            this._selState = value;
+        }
+        get
+        {
+            return this._selState;
+        }
+    }
+    public string AddressType
+    {
+        set
+        {
+            this._addressType = value;
+        }
+        get
+        {
+            return this._addressType;
+        }
+    }
 
 }
---
+++ Group2.Znalytics_GreatOutDoors.PresentationLayer/ReturnsPresentation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.Znalytics_GreatOutDoors.PresentationLayer
+{
+    class ReturnsPresentation
+    {
+        static void Main()
+        {
+          // public static void ReturnsPresentationLogic()
+            //{
+                ReturnsModule r;
+                r = new ReturnsModule();
+                int choice;
+                System.Console.WriteLine("Select your reason ");
+                System.Console.WriteLine("______________________");
+                System.Console.WriteLine("1. Unsatisfactory Product");
+                System.Console.WriteLine("2. Defective Product");
+                System.Console.WriteLine("3. Incomplete Product");
+                System.Console.WriteLine("4. Wrong Product Ordered");
+                System.Console.WriteLine("5. Wrong Product Shipped");
+
+                System.Console.WriteLine(" Enter your Choice");
+                choice = System.Convert.ToInt32(System.Console.ReadLine());
+                //switch case to select reasons
+                switch (choice)
+                {
+                    case 1:
+                        System.Console.WriteLine(" Unsatisfactory Product");
+                        break;
+                    case 2:
+                        System.Console.WriteLine("Defective Product");
+                        break;
+                    case 3:
+                        System.Console.WriteLine("Incomplete Product");
+                        break;
+                    case 4:
+                        System.Console.WriteLine("Wrong Product Ordered");
+                        break;
+                    case 5:
+                        System.Console.WriteLine(" Wrong Product Shipped");
+                        break;
+                }
+
+
+
+                System.Console.WriteLine("Enter ProductID");
+                r.ProductID = (System.Console.ReadLine());
+                System.Console.WriteLine(r.ProductID);
+
+                System.Console.WriteLine("Enter ProductName");
+                r.ProductName = System.Console.ReadLine();
+                System.Console.WriteLine(r.ProductName);
+
+                System.Console.WriteLine("Enter ProductDate");
+                r.Productdate = (System.Console.ReadLine());
+                System.Console.WriteLine(r.Productdate);
+
+                System.Console.WriteLine("Enter ProductQuantity");
+                r.ProductQuantity = (System.Convert.ToInt32(System.Console.ReadLine()));
+                System.Console.WriteLine(r.ProductQuantity);
+
+                System.Console.WriteLine("Enter Producttype");
+                r.Producttype = (System.Console.ReadLine());
+                System.Console.WriteLine(r.Producttype);
+
+
+                System.Console.WriteLine("--------------- Deatils are --------------");
+                System.Console.WriteLine("Product Id: " + r.ProductID);
+                System.Console.WriteLine("Employee Name: " + r.ProductName);
+                System.Console.WriteLine("number of hours you worked: " + r.Productdate);
+                System.Console.WriteLine("Hourly price of yours: " + r.ProductQuantity);
+                System.Console.WriteLine("Gross Salary of your is: " + r.Producttype);
+                System.Console.WriteLine("ThankYou for your Response");
+                System.Console.ReadKey();
+
+
+                // System.Collections.Generic.List<ProductID> products = new System.Collections.Generic.List<ProductID>();
+                // products.Add(ProductID);
+            }
+       // }
+    }
+}
