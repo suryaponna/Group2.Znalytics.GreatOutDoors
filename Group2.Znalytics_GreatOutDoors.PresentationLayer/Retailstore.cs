fdfe48a8b9c86673f7b929fbe738218f6636ef2b
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-17 08:10:58 +0530

    retail (class)

--- Group2.Znalytics_GreatOutDoors.PresentationLayer/Retailstore.cs
+++ Group2.Znalytics_GreatOutDoors.PresentationLayer/Retailstore.cs
@@ -1,139 +1,89 @@
-﻿using System.Reflection.Emit;
-using System.Security.AccessControl;
-
-class Address
+﻿class Program
 {
     static void Main()
     {
-    NewAddress:
-        int spec = 0;
-        AddressDetails ad = new AddressDetails();
-        System.Console.WriteLine("------------------Enter Your Shipping Details ------------------");
-        System.Console.WriteLine("Enter Country:");
-        ad.CustomerCountry = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter Your Name:");
-        ad.CustomerName = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter Mobile Number:");
-        ad.MobileNumber = (System.Convert.ToInt64(System.Console.ReadLine()));
-        System.Console.WriteLine("Enter Your Pincode:");
-        ad.PinCode = (System.Convert.ToInt64(System.Console.ReadLine()));
-        System.Console.WriteLine("Enter Flat/DoorNumber:");
-        ad.FlatNo = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter Your Area");
-        ad.AreaColony = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter your LandMark:");
-        ad.LandMark = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter your Town/City:");
-        ad.Town = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter your State");
-        ad.State = (System.Console.ReadLine());
-        System.Console.WriteLine("Enter your Address Type");
-        ad.AddressType = (System.Console.ReadLine());
-    Add:
-        System.Console.WriteLine("These are your Address Details");
-        System.Console.WriteLine("Country: " + ad.CustomerCountry);
-        System.Console.WriteLine("Name: " + ad.CustomerName);
-        System.Console.WriteLine("Mobile Number: " + ad.MobileNumber);
-        System.Console.WriteLine("PinCode: " + ad.PinCode);
-        System.Console.WriteLine("FlatNo " + ad.FlatNo);
-        System.Console.WriteLine("Area: " + ad.AreaColony);
-        System.Console.WriteLine("LandMark: " + ad.LandMark);
-        System.Console.WriteLine("Town/City: " + ad.Town);
-        System.Console.WriteLine("State: " + ad.State);
-        System.Console.WriteLine("Address Type: " + ad.AddressType);
-        if (spec == 1)
-        {
-            goto Lab;
-        }
+        Retailstore r = new Retailstore();
+        System.Console.WriteLine("********WELCOME TO RETAIL STORE********");
 
-        System.Console.WriteLine("If you want to add new address enter 1");
-        System.Console.WriteLine("If you want to modify  address enter 2");
-        System.Console.WriteLine("Enter 3 to Finalize the adress ");
-        int a = System.Convert.ToInt32(System.Console.ReadLine());
-        if (a == 3)
-        {
-            goto Lab;
-        }
-        if (a == 1)
-        {
-            System.Console.WriteLine("Enter New Address Details");
-            goto NewAddress;
-        }
-        else
-        {
-            System.Console.WriteLine("Select your Fileds to modify");
-            while (true)
-            {
-                System.Console.WriteLine("For Country enter 1");
-                System.Console.WriteLine("For Name enter 2");
-                System.Console.WriteLine("For Mobile Number enter 3");
-                System.Console.WriteLine("for PinCode Enter 4 ");
-                System.Console.WriteLine("For FlatNo Enter 5");
-                System.Console.WriteLine("For Area Enter 6");
-                System.Console.WriteLine("For LandMark Enter 7");
-                System.Console.WriteLine("For Town/City Enter 8");
-                System.Console.WriteLine("For State Enter 9");
-                System.Console.WriteLine("For Address Type Enter 10 ");
-                System.Console.WriteLine("To Finish Modifiying Enter 11");
-                int b = int.Parse(System.Console.ReadLine());
-                switch (b)
-                {
-                    case 1:
-                        System.Console.WriteLine("Enter Country:");
-                        ad.CustomerCountry = (System.Console.ReadLine());
-                        break;
+        //retail id
+        System.Console.WriteLine("Enter The Retail store Id");
+        r.RetailStoreID = (System.Convert.ToInt32(System.Console.ReadLine()));
+        System.Console.WriteLine("The retail store Id is:" + r.RetailStoreID);
 
-                    case 2:
-                        System.Console.WriteLine("Enter Your Name:");
-                        ad.CustomerName = (System.Console.ReadLine());
-                        break;
-                    case 3:
-                        System.Console.WriteLine("Enter Mobile Number:");
-                        ad.MobileNumber = (System.Convert.ToInt64(System.Console.ReadLine()));
-                        break;
-                    case 4:
-                        System.Console.WriteLine("Enter Your Pincode:");
-                        ad.PinCode = (System.Convert.ToInt64(System.Console.ReadLine()));
-                        break;
-                    case 5:
-                        System.Console.WriteLine("Enter Flat/DoorNumber:");
-                        ad.FlatNo = (System.Console.ReadLine());
-                        break;
-                    case 6:
-                        System.Console.WriteLine("Enter Your Area");
-                        ad.AreaColony = (System.Console.ReadLine());
-                        break;
-                    case 7:
-                        System.Console.WriteLine("Enter your LandMark:");
-                        ad.LandMark = (System.Console.ReadLine());
-                        break;
-                    case 8:
-                        System.Console.WriteLine("Enter your Town:");
-                        ad.Town = (System.Console.ReadLine());
-                        break;
-                    case 9:
-                        System.Console.WriteLine("Enter your State:");
-                        ad.State = (System.Console.ReadLine());
-                        break;
-                    case 10:
-                        System.Console.WriteLine("Enter your Address Type:");
-                        ad.AddressType = (System.Console.ReadLine());
-                        break;
-                    case 11:
-                        System.Console.WriteLine("---------Mofification are finished-------");
-                        spec = 1;
-                        goto Add;
-                    default:
-                        System.Console.WriteLine("Enter valid Option");
-                        break;
-
-                }
-
-            }
+        //choice of choosing reports
+        System.Console.WriteLine("*****Select the choice  for payment choosing reports******");
+        System.Console.WriteLine("Enter 1 for Monthly reports");
+        System.Console.WriteLine("Enter 2 for Quaterly reports");
+        System.Console.WriteLine("Enter 3 for Annual reports");
+        int Choice = int.Parse(System.Console.ReadLine());
+        switch (Choice)
+        {
+            case 1:
+                System.Console.WriteLine("Monthly reports");
+                r.Reports = "Monthly reports";
+                break;
+            case 2:
+                System.Console.WriteLine("Quarterly reports");
+                r.Reports = "Quarterly reports";
+                break;
+            case 3:
+                System.Console.WriteLine("Annual reports");
+                r.Reports = "Annual Reports";
+                break;
+            default:
+                System.Console.WriteLine("Unknown choice");
+                break;
         }
-    Lab:
-        System.Console.WriteLine("----------Thank You for For your Interest to Buy the Product----------------- ");
+        System.Console.WriteLine("Selected reports are:" + r.Reports);
         System.Console.ReadKey();
+
+
+        //retail products
+        System.Console.WriteLine("******choice of choosing types of products******");
+        System.Console.WriteLine("Enter 1 for Electrical Appliances");
+        System.Console.WriteLine("Enter 2 for Grocery products");
+        System.Console.WriteLine("Enter 3 for Medicines");
+        System.Console.WriteLine("Enter 4 for Fashion products");
+        System.Console.WriteLine("Enter 5 for Fitness products");
+        System.Console.WriteLine("Enter 6 for Industrial products");
+        int b = int.Parse(System.Console.ReadLine());
+        while (true)
+        {
+            switch (b)
+            {
+                case 1:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = "Electrical Appliances";
+                    break;
+                case 2:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = "Grocery products";
+                    break;
+                case 3:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = " Medicines";
+                    break;
+                case 4:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = "Fashion products";
+                    break;
+                case 5:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = "Fitness products";
+                    break;
+                case 6:
+                    r.RetailProducts = System.Console.ReadLine();
+                    r.RetailProducts = "Industrial products";
+                    break;
+                default:
+                    System.Console.WriteLine("Enter Valid option");
+                    break;
+            }
+            System.Console.WriteLine("Selected the type of Retail products are:" + r.RetailProducts);
+            System.Console.ReadKey();
+
+        }
+       // System.Console.WriteLine();
 
     }
 
