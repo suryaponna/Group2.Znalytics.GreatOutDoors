5ac439a8ed6db7382cd2f5f19285223dfdc39fe7
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-18 08:05:31 +0530

    retail(data)

---
+++ Group2.Znalytics.GreetOutDoors.DataLayer/RetailStoreDataLayaer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.Znalytics.GreetOutDoors.DataLayer
+{
+    public class RetailStoreDataLayaer
+    {
+        List<string> DataBase = new List<string>();
+        public void AddAddress(string retailStore)
+        {
+            DataBase.Add(retailStore);
+        }
+    }
+}
