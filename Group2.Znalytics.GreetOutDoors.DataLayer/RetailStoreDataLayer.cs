ca7f7eef47a70c712aade2b90c17f06adf1fb115
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-19 18:54:04 +0530

    retail

--- Group2.Znalytics.GreetOutDoors.DataLayer/RetailStoreDataLayer.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/RetailStoreDataLayer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /// <summary>
-/// Data Access Logic for RetailStore
+/// Data
 /// </summary>
 
 namespace Group2.Znalytics.GreatOutDoors.DataLayer
