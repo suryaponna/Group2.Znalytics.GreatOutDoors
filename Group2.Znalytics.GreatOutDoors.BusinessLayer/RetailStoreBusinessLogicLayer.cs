b72a5698c0a4e897b00213ef4376f698ca99a8d8
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-20 08:21:12 +0530

    business,data

--- Group2.Znalytics.GreatOutDoors.BusinessLayer/RetailStoreBusinessLogicLayer.cs
+++ Group2.Znalytics.GreatOutDoors.BusinessLayer/RetailStoreBusinessLogicLayer.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using Group2.Znalytics.GreatOutDoors.DataLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Business Logic for RetailStore
+/// </summary>
 namespace Group2.Znalytics.GreatOutDoors.BusinessLayer
 {
-    class RetailStoreBusinessLogicLayer
+   public class RetailStoreBusinessLogicLayer
     {
+       // RetailStoreDataLayer dl = new RetailStoreDataLayer();
+       // RetailStoreadd ad = new RetailStoreadd();
+        
+        public void AddRetailStoreID(RetailStore rs)
+        {
 
+            if (rs.RetailStoreID.Length != null)
+            {
+                RetailStoreDataLayer rdl = new RetailStoreDataLayer();
+                rdl.AddRetailStoreID(rs);
+                
+            }
+           
+        }
     }
 }
