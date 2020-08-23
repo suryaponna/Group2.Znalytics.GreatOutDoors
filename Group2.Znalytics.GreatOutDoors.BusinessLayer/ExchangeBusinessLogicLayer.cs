d47ee5a9873d197f814d617abb7fecc6cfd0806d
Anitha <anithasweety784@gmail.com>
2020-08-23 23:17:34 +0530

    11.17

---
+++ Group2.Znalytics.GreatOutDoors.BusinessLayer/ExchangeBusinessLogicLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Group2.Znalytics.GreatOutDoors.BusinessLayer
+{
+    public class ExchangeBusinessLogicLayer
+    {
+        private ExchangeBusinessLogicLayer exchangeBusinessLogicLayer;
+        public ExchangeBusinessLogicLayer()
+        {
+
+        }
+    }
+       
