bc0d567a22911e19833cf94ee94f6028ac1d98a9
Anitha <anithasweety784@gmail.com>
2020-08-27 01:27:52 +0530

    1.27

---
+++ Group2.Znalytics.GreatOutDoors.EntityLayer/OrderEntityLayer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Group2.Znalytics.GreatOutDoors.EntityLayer
+{
+    /// <summary>
+    /// Represents details of Order
+    /// </summary>
+   public class OrderEntityLayer
+    {
+        //private fields
+        private string _customerName;
+        private string _productID;
+        private string _quantity;
+        private string _address;
+        private string _addToCart;
+        private string _paymentprocess;
+        /// <summary>
+        /// Constructor that initializes the details of Customer
+        /// </summary>
+        /// <param name="customerName"></param>
+        /// <param name="productID"></param>
+        /// <param name="address"></param>
+        /// <param name="quantity"></param>
+        /// <param name="addToCart"></param>
+        /// <param name="paymentprocess"></param>
+        public void Customer(string customerName,string productID,string address,string quantity,string addToCart,string paymentprocess)
+        {
+            _customerName = customerName;
+            _productID = productID;
+            _address = address;
+            _quantity = quantity;
+            _addToCart = addToCart;
+            _paymentprocess = paymentprocess;
+        }
+        /// <summary>
+        /// Initializes the property of customerName
+        /// </summary>
+        public string CustomerName
+        {
+            set
+            {
+                if(value.Length <= 10)
+                {
+                    this._customerName = value;
+                    else
+                        throw new Exception("Your name exceeded the size that maximum characters are 14");
+                }
+            }
+            get
+            {
+                return _customerName;
+            }
+        }
+
+        public string ProductID
+        {
+            set
+            {
+                if(value.Length <=20)
+                {
+                    this._productID = value;
+                }
+
+            }
+            get
+            {
+                return _productID;
+            }
+        }
+        public string Address
+        {
+            set
+            {
+                if(value.Length<=50)
+                {
+                    this._address = value;
+                }
+            }
+            get
+            {
+                return _address;
+            }
+        }
+        public string AddToCart
+        {
+            set
+            {
+                if(value.Length<30)
+                {
+                    this._addToCart = value;
+                }
+            }
+            get
+            {
+                return _addToCart;
+            }
+        }
+        public string PaymentProcess
+        {
+            set
+            {
+                if(value.Length<=30)
+                {
+                    this._paymentprocess = value;
+                }
+            }
+            get
+            {
+                return _paymentprocess;
+            }
+        }
+    }
+
+}
---
+++ Group2.Znalytics.GreetOutDoors.DataLayer/OrderDataLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.Znalytics.GreetOutDoors.DataLayer
+{
+    class OrderDataLayer
+    {
+        //automatic property (or) auto-implemented property
+        public string customerName { set; get; }
+        public string productID { get; set; }
+        public string address { get; set; }
+        public string addToCart { get; set; }
+        public string paymentProcess { get; set; }
+    }
+}
