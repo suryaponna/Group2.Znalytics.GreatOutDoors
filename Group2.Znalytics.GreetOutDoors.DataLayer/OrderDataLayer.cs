be1b781d4468ccd90de09988f593367cd242f786
Anitha <anithasweety784@gmail.com>
2020-08-27 18:26:14 +0530

    18.26

--- Group2.Znalytics.GreetOutDoors.DataLayer/OrderDataLayer.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/OrderDataLayer.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Znalytics.Group2.GreatOutDoor.Entity;
 
 namespace Group2.Znalytics.GreetOutDoors.DataLayer
 {
     class OrderDataLayer
     {
-        //automatic property (or) auto-implemented property
-        public string customerName { set; get; }
-        public string productID { get; set; }
-        public string address { get; set; }
-        public string addToCart { get; set; }
-        public string paymentProcess { get; set; }
+
+
+
+        AddressDataLayer ad= new AddressDataLayer();  
+    }
+
+        
     }
 }
