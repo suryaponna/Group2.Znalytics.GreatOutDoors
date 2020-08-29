6c0988230e6bc5eaccebec5889ec434d63fe54e4
SuryaPonna <suryaponna0@gmail.com>
2020-08-29 09:57:07 +0530

    comit

--- Group2.Znalytics.GreetOutDoors.DataLayer/AddressDataLayer.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/AddressDataLayer.cs
@@ -6,6 +6,7 @@ using System.IO;
 using Group2.Znalytics.GreetOutDoors.DataLayer;
 using Group2.Znalytics.GreatOutDoors.EntityLayer;
 using Newtonsoft.Json;
+using System.Linq;
 /// <summary>
 /// Data Access Logic for Address
 /// </summary>
@@ -192,11 +193,7 @@ namespace Znalytics.Group2.GreatOutDoor.Entity
         /// <returns></returns>
         public AddressDetail ReturnAddress(AddressDetail ad) {
             AddressDetail samp=null;
-            foreach (var temp in _customerAddressesList) {
-                if (temp.CustomerId == ad.CustomerId && temp.AddressId==ad.AddressId) {
-                    samp = temp;
-                }
-            }
+            samp = _customerAddressesList.Find(temp=> temp.AddressId==ad.AddressId && temp.CustomerId==ad.CustomerId);
             if (samp != null)
             {
                 return samp;
