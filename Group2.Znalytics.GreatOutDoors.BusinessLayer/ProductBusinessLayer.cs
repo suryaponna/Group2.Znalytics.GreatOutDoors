636199c81fb41c27e541d0836cc82482d8ad02b0
meghana533 <meghananimishakavi199@gmail.com>
2020-08-29 09:09:35 +0530

    cfg

--- Group2.Znalytics.GreatOutDoors.BusinessLayer/ProductBusinessLayer.cs
+++ Group2.Znalytics.GreatOutDoors.BusinessLayer/ProductBusinessLayer.cs
@@ -13,148 +13,19 @@ namespace GreatOutdoorsProduct.BusinessLayer
     {
         private static bool Product(Product product)
 
+    /// <summary>
+    /// refers tp Product BusinessLogic
+    /// </summary>
+    public class ProductBusiness
+    {
+
+        /// <summary>
+        /// adding the products 
+        /// </summary>
+        /// <param name="n"> gets object of ProductDetails</param>
+        public void AddProducts(ProductDetails n)
         {
-            StringBuilder sb = new StringBuilder();
-            bool validProduct = true;
-            if (product.ProductID <= 0)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Invalid Product ID");
-
-            }
-            if (product.ProductName == string.Empty)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Product Name Required");
-
-            }
-            if (product.CategoryID <= 0)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Invalid Category ID");
-
-            }
-
-            if (product.SpecificationID <= 0)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Invalid Specification ID");
-
-            }
-
-            if (product.CostPrice <= 0)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Invalid Cost Price");
-
-            }
-
-            if (product.SellingPrice <= 0)
-            {
-                validProduct = false;
-                sb.Append(Environment.NewLine + "Invalid Selling Price");
-
-            }
-
-            if (validProduct == false)
-                throw new ProductException(sb.ToString());
-            return validProduct;
-        }
-
-        public static bool AddProductBusinessLayer(Product newProduct)
-        {
-            bool productAdded = false;
-            try
-            {
-                if (Product(newProduct))
-                {
-                    ProductDataAccessLayer ProductDataAccessLayer = new ProductDataAccessLayer();
-                    productAdded = ProductDataAccessLayer.AddProductDataAccessLayer(newProduct);
-                }
-            }
-            catch (ProductException)
-            {
-                throw;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            return productAdded;
-        }
-
-        public static List<Product> GetAllProductsBusinessLogicLayer()
-        {
-            List<Product> productList = null;
-            try
-            {
-                ProductDataAccessLayer ProductDataAccessLayer = new ProductDataAccessLayer();
-                productList = ProductDataAccessLayer.GetAllProductsDataAccessLayer();
-            }
-            catch (ProductException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            return productList;
-        }
-
-        public static List<Product> GetProductsByNameBusinessLogicLayer(string productName)
-        {
-            List<Product> productList = null;
-            try
-            {
-                ProductDataAccessLayer ProductDataAccessLayer = new ProductDataAccessLayer();
-                productList = ProductDataAccessLayer.GetProductsByNameDataAccessLayer(productName);
-                // ProductDataAccessLayer ProductDataAccessLayer = new ProductDataAccessLayer();
-                //productList = ProductDataAccessLayer.AddProductDataAccessLayer(ProductName);
-            }
-            catch (ProductException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            return productList;
-        }
-
-        public static List<Product> GetProductsByCategoryBusinessLogicLayer(int categoryID)
-        {
-            List<Product> productList = null;
-            try
-            {
-                ProductDataAccessLayer productDataAccessLayer = new ProductDataAccessLayer();
-                productList = productDataAccessLayer.GetProductsByCategoryDataAccessLayer(categoryID);
-            }
-            catch (ProductException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            return productList;
-        }
-        public static Product SearchProductBL(int searchProductID)
-        {
-            Product searchProduct = null;
-            try
-            {
-                ProductDataAccessLayer productDataAccessLayer = new ProductDataAccessLayer();
-                searchProduct = productDataAccessLayer.SearchProductDataAccessLayer(searchProductID);
-            }
-            catch (ProductException ex)
-            {
-                throw ex;
-            }
-            catch (Exception ex)
+            if (n.ProductName != null)
             {
                 throw ex;
             }
