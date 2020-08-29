bd12adfc1b142f652a4dec157e53b3252368457e
Archana-byte-1234 <archana.mmerugu@gmail.com>
2020-08-29 09:54:33 +0530

    updated

--- Group2.Znalytics.GreetOutDoors.DataLayer/RetailStoreDataLayer.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/RetailStoreDataLayer.cs
@@ -26,13 +26,13 @@ namespace Group2.Znalytics.GreatOutDoors.DataLayer
         //GET retailstore id
         public List<Retailstore> GetRetailStoreIDs()
         {
-            throw new Exception("Enter valid retailId");
+            throw new RetailstoreException("Enter valid retailId");
         }
 
         //Method to add product qunatity to the
         public void AddProductQuantity(Retailstore p)
         {
-            throw new Exception("Enter product quantity");
+            throw new RetailstoreException("Enter product quantity");
 
         }
     }
