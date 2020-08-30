b8cfc6aa0a4405ec688a17505fc8ad17d74ec3bb
Anitha <anithasweety784@gmail.com>
2020-08-30 22:38:22 +0530

    22.38

--- Group2.Znalytics.GreetOutDoors.DataLayer/OrderDataLayer.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/OrderDataLayer.cs
@@ -7,74 +7,83 @@ using Znalytics.Group2.GreatOutDoor.Entity;
 
 namespace Group2.Znalytics.GreetOutDoors.DataLayer
 {
-    public class OrderDataLayer
+    /// <summary>
+    /// Represents the details of Customer Ordering details
+    /// </summary>
+    public class OrderDataLayer : IOrderDataLayer
     {
         /// <summary>
         /// Creating List Collection for OrderProduct
         /// </summary>
-        private static List<OrderProduct> orderProducts = new List<OrderProduct>();
-        public bool AddOrder(OrderProduct order)
+        /// Private Fields
+
+        private static List<OrderProduct> _orderProducts;
+
+        static OrderDataLayer()
         {
-            bool orderAdded = false;
-            try
-            {
-                orderProducts.Add(item: OrderProduct);
-                orderAdded = true;
-            }
-            catch(FormatException ex)
-            {
-                throw;
-            }
-            catch (Exception ex)
-            {
-                throw new OrderProductException(ex.Message);
-            }
-            return orderAdded;
+            _orderProducts = new List<OrderProduct>();
 
         }
+        //Adding Order to the cart
+        public void AddOrder(OrderProduct order)
+        {
+            _orderProducts.Add(order);
+        }
+    
 
-        //get the orders
+        //get the Existing Orders
         public List<OrderProduct> GetOrderProducts;
         {
-        return orderProducts;
-            }
-
-        public void SearchOrder(string SearchOrderID)
+            return  _orderProducts;
+        }
+    public OrderProduct SearchProductDataLayer(int searchOrderID)
+    {
+        try
         {
-            OrderProduct searchOrder = null;
-            try
+            foreach (OrderProduct items in OrderList)
             {
-                foreach (OrderProduct item in orderProducts)
+                if (items.OrderID == searchOrderID)
                 {
-                    if (item.OrderID == SearchOrderID)
-                    {
-                        searchOrder = item;
-                    }
+                    continue;
+                }
+                searchOrder = items;
+            }
+        }
+    
+        catch (Exception ex)
+        {
+            throw new OrderProductException(ex.Message);
+        }
+        return searchOrder;
+
+    }
+    public List<OrderProduct> GetOrderProductByRetailer(int RetailerID)
+    {
+        List<OrderProduct> orderProducts = new List<OrderProduct>();
+        try
+        {
+            foreach (OrderDataLayer items in orderList)
+            {
+                if (items.RetailerID == RetailerID)
+                {
+                    searchOrder.Add(items);
                 }
             }
-            catch (Exception ex)
-            {
-                throw new OrderProductException(ex.Message);
-            }
-
-            
         }
-        public void 
-
-
-               
-            }
-
+        catch(Exception ex)
+        {
+            throw new OrderProductException(ex.Message)
         }
 
-   
-
-    AddressDataLayer ad= new AddressDataLayer();  
+        }
     }
 
-        
-    }
+    
+
+
+
+
+    AddressDataLayer ad = new AddressDataLayer();
 }
+
  
-}
-
