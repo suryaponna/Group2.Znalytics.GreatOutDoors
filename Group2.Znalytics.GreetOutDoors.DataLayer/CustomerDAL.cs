c342bbd0699caa4fecf97da1873eecfa36e2b2de
Apoorva0701 <apoorva.ramyshetty0701@gmail.com>
2020-08-31 12:59:40 +0530

    1:00pm

--- Group2.Znalytics.GreetOutDoors.DataLayer/CustomerDAL.cs
+++ Group2.Znalytics.GreetOutDoors.DataLayer/CustomerDAL.cs
@@ -1,31 +1,42 @@
-﻿using Group2.Znalytics.GreatOutDoor.EntityLayer;
+﻿using Group2.Znalytics.GreatOutDoors.EntityLayer;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-
-//using Group2.Znalytics.GreatOutDoors.EntityLayer;
+using System.Linq;
+using Group2.Znalytics.GreetOutDoors.DataLayer;
 
 namespace Group2.Znalytics.GreatOutDoors.DataLayer
 
 {
     /// <summary>
-    /// Represents Data Access Layer of  the customer Personal details
+    /// Represents Data Access Layer of  the customer details
     /// </summary>
     public class CustomerDAL : ICustomerDAL
     {
         //private fields
         private static List<Customer> _customers;
-
+        //static field of DataccessLayer
         static CustomerDAL()
         {
             _customers = new List<Customer>();
+
+            //code for loading data from file into _customers
         }
 
         //Adding Customer Personal Details
         public void AddCustomer(Customer customer)
         {
             //generate new customer id
-            //customer.CustomerId = ??
+            if (_customers.Count == 0)
+            {
+                customer.CustomerId = 1;
+            }
+            else
+            {
+                customer.CustomerId = _customers.Max(temp => temp.CustomerId) + 1;
+            }
+
+            //add customer object to the collection
             _customers.Add(customer);
         }
 
@@ -40,9 +51,9 @@ namespace Group2.Znalytics.GreatOutDoors.DataLayer
         {
             //Get matching customer based on CustomerId
             Customer cust = _customers.Find(temp => temp.CustomerId == customer.CustomerId);
-            if (customer != null)
+            if (cust != null)
             {
-                customer.CustomerName = customer.CustomerName;
+                cust.CustomerName = customer.CustomerName;
             }
         }
 
@@ -51,13 +62,13 @@ namespace Group2.Znalytics.GreatOutDoors.DataLayer
         /// </summary>
         /// <param name="customerID">CustomerID to search</param>
         /// <returns>Returns matching customer</returns>
-        public Customer GetCustomerByCustomerID(string customerID)
+        public Customer GetCustomerByCustomerID(string CustomerId)
         {
-            Customer cust = _customers.Find(temp => temp.CustomerID == customerID);
+            Customer cust = _customers.Find(temp => temp.CustomerId == CustomerId);
             return cust;
-            ListOfCustomers();
-        }
 
+        }
+        //JSON is used for converting object to text format
         public void ListOfCustomers()
         {
             List<Customer> _return = new List<Customer>();
@@ -81,13 +92,17 @@ namespace Group2.Znalytics.GreatOutDoors.DataLayer
         {
             List<Customer> cust = _customers.FindAll(temp => temp.CustomerName == customerName);
             return cust;
-            ListOfCustomers();
+            
         }
-    }
-    public void DeleteCustomers(Customer customers)
-    {
-        _customers.Remove(temp=>CustomerName==Customers.CustomerName);
-       
-    }
+
+        public void DeleteCustomer(string customerID)
+        {
+            _customers.RemoveAll(temp => temp.CustomerId == customerID);
+        }
+
+     }
 }
+
+
+
 
